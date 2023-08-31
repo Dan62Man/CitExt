1d8a386ee13e3af129cc925dbacea133e2e5b77d
Daniel Grgic Figueiredo <danielgf98@gmail.com>
2023-08-31 17:34:51 +0200

    updated code

--- README.md
+++ README.md
@@ -1,9 +1,24 @@
 # CitExt
 Citation extractor from text, finds sentences with citations and the enclosing sentences.
+It then provides a sentiment analysis, along with NLI indicating whether the citation section (part of text mentioning and discussing the citation) is positive negative or neutral finally these are represented in a graph, where the colour red indicates a negative reference, the colour green indicates a positive one. Otherwise it is neutral
 
 ## Needed information
 
-Save your text in .txt files and save them in a `texts` folder.
+Save your text in .txt format and save them in a `texts` folder.
+Provide a .txt with the same base name of the file abve followed by `-bibliography.txt`:
+```
+texts
+ |
+ -- KentEtAl2023.txt
+ -- KentEtAl2023-bibliography.txt:
+```
+
+Contents KentEtAl2023-bibliography.txt:
+```
+Clark Kent and Bruce Wayne. 2023. Example of a reference in bibliography
+M. Morales, P. Parker, Dr O. Octavius. 2023. Second Line of example
+... 
+```
 
 
 ## Output
--- code/citation_data_manipulation.py
+++ code/citation_data_manipulation.py
@@ -11,12 +11,11 @@ def get_title_from_ref(ref):
 
   if '\"' in ref:
     title = ref.split('\"')[1]
-    print(title)
     return title
 
   for part in parts:
     find_names = ner(part.strip())
-    if any( element['entity_group'] == 'PER' for element in find_names):
+    if any(((element['entity_group'] == 'PER') & (element['word'] in part)) for element in find_names):
       continue
     if len(part.split()) >= 3:
       return part
@@ -28,19 +27,19 @@ def get_author_with_ner(ref):
   for entity in entities:
     if entity['entity_group'] == 'PER':
       author = entity['word']
-      return author, author
+      return author.strip(), author.strip()
   
   if ' and ' in ref:
     if ',' in ref:
       ref_author = ref.split(',')[0]
       author = ref.replace(',', '')
-      return ref_author, author
+      return ref_author.strip(), author.strip()
     ref_author = ref.split(' and ')[0]
-    return ref_author, ref
+    return ref_author.strip(), ref.strip()
   
   author = re.sub(r'[,()]', '', ref) 
 
-  return author, author
+  return author.strip(), author.strip()
 
 def append_reference_data(paper_data, text_id, citation):
   bibl_file = "./texts/" + text_id + "-bibliography.txt"
@@ -53,11 +52,18 @@ def append_reference_data(paper_data, text_id, citation):
   year = 0
 
   for ref in refs:
-    if ref[0] == citation or citation == ref[1]:
-      title = get_title_from_ref(ref)
-      year = re.search(r"\d{4}", ref).group()
-      ref_author, author = get_author_with_ner(ref)
-      break
+    if len(citation) == 1:
+      if ref[0] == citation or citation == ref[1]:
+        title = get_title_from_ref(ref)
+        year = re.search(r"\d{4}", ref).group()
+        ref_author, author = get_author_with_ner(ref)
+        break
+    elif len(citation) == 2:
+      if re.search(r"\d{2}", ref).group() == citation:
+        title = get_title_from_ref(ref)
+        year = re.search(r"\d{4}", ref).group()
+        ref_author, author = get_author_with_ner(ref)
+        break
     elif len(citation) >= 3:
       year = re.search(r"\d{4}", citation).group()
       if year in ref:
@@ -73,10 +79,10 @@ def append_reference_data(paper_data, text_id, citation):
 
   global_id = len(paper_data)
 
-  author = re.sub(r'[^A-Za-z ]', '', author).replace(" and ","").replace(" ", "")
-  id = f"{author}EtAl{year}"
-  if 'And' in author:
-    id = f"{author}{year}"
+  id_author = re.sub(r'[^A-Za-z ]', '', author).replace(" and ","And").replace(" ", "")
+  id = f"{id_author}EtAl{year}"
+  if 'And' in id_author:
+    id = f"{id_author}{year}"
   title = re.sub(r'^\s', '', title)
   paper_data[global_id] = {
     "ID": id,
