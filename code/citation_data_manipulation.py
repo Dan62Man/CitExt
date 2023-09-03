04223d52f96a822bfcf36f84003efe03ca2b27d5
Daniel Grgic Figueiredo <danielgf98@gmail.com>
2023-09-03 17:01:45 +0200

    code better at finding elements

--- code/citation_data_manipulation.py
+++ code/citation_data_manipulation.py
@@ -1,9 +1,27 @@
 import re
 from sys import stdin
 import os
+import string
 from transformers import pipeline
 
 ner = pipeline("token-classification", model="Jean-Baptiste/roberta-large-ner-english", aggregation_strategy="simple")
+
+def get_global_id(paper_data, paper_id, author, year, title):
+  cnt = 0
+  alphabet = string.ascii_lowercase
+  for paper in paper_data:
+    if paper == paper_id:
+      data = paper_data[paper]
+      if data["Title"] == title:
+        return paper
+      else:
+        cnt += 1
+  
+  if cnt == 0:
+    return paper_id
+  
+  return f"{paper_id}{alphabet[cnt]}"
+
 
 def get_title_from_ref(ref):
   entities = ner(ref.strip())
@@ -27,8 +45,20 @@ def get_author_with_ner(ref):
   for entity in entities:
     if entity['entity_group'] == 'PER':
       author = entity['word']
+      if author[0] == '.':
+        author = author[1:]
+
+      if '.' in author:
+        possible_author = author.split('.')
+
+        for name in possible_author:
+          if len(name.strip()) > 1:
+            return name.strip(), author.strip()
+      elif ' and ' in author:
+        return author.split(' and ')[0].strip(), author
+
       return author.strip(), author.strip()
-  
+
   if ' and ' in ref:
     if ',' in ref:
       ref_author = ref.split(',')[0]
@@ -49,6 +79,7 @@ def append_reference_data(paper_data, text_id, citation):
   refs = text.split('\n')
   title = ""
   author = ""
+  ref_author = ""
   year = 0
 
   for ref in refs:
@@ -68,24 +99,20 @@ def append_reference_data(paper_data, text_id, citation):
       year = re.search(r"\d{4}", citation).group()
       if year in ref:
         ref_author, author = get_author_with_ner(citation.replace(year, ''))
-        if author in ref:
+        if ref_author in ref:
           title = get_title_from_ref(ref)
           break
 
-  for paper in paper_data:
-    data = paper_data[paper]
-    if str(year) == data["Year"] and author.strip() == data["Author"]:
-      return paper
-
-  global_id = len(paper_data)
-
-  id_author = re.sub(r'[^A-Za-z ]', '', author).replace(" and ","And").replace(" ", "")
+  id_author = re.sub(r'[^A-Za-z ]', '', ref_author).replace(" and ","And").replace(" ", "")
   id = f"{id_author}EtAl{year}"
   if 'And' in id_author:
     id = f"{id_author}{year}"
-  title = re.sub(r'^\s', '', title)
+  title = title.strip()
+      
+
+  global_id = get_global_id(paper_data, id, author, year, title)
+
   paper_data[global_id] = {
-    "ID": id,
     "Author": author,
     "Title": title,
     "Year": year
